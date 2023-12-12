081a289b58802bd99e6c5cba98da6e2105626b92
Kaio Vinícios <kvsgpro@outlook.com>
2023-12-11 21:47:03 -0300

    Prática Python 11/12/2023.

---
+++ Python/11_12_23.py
@@ -0,0 +1,10 @@
+# 8 kyu 'You Can't Code Under Pressure #1' by Kerric
+# Objective: Double the integer and return it!
+
+def double_integer(i):
+    pass # Double the integer and return it!
+    if i % 1 == 0:
+        i *= 2
+        return i
+    else:
+        return print('there is not an integer.')
