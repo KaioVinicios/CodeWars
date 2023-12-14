63915385a5f8d1829e9b849b21e3ce499d088091
Kaio Vinícios <kvsgpro@outlook.com>
2023-12-14 20:08:21 -0300

    Prática JavaScript 14/12/2023.

---
+++ JavaScript/14_12_23.js
@@ -0,0 +1,8 @@
+// 8 kyu 'Convert a Number to a String!' by AKJ.IO.
+// Objective: 'Convert and return any integer to a string.'
+
+function numberToString(num) {
+    // Return a string of the number here!
+    result = String(num) 
+    return result
+  }
