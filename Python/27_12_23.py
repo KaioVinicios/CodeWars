41f670f6c902cb559ff11a176a1666ced94a9eda
Kaio Vinícios <kvsgpro@outlook.com>
2023-12-28 17:53:59 -0300

    Prática Python 27/12/2023.

---
+++ Python/27_12_23.py
@@ -0,0 +1,6 @@
+''' 8 kyu 'Remove String Spaces' by PG1
+Write a function that removes the spaces from the string, then return the resultant string.'''
+
+def no_space(x):
+    #your code here
+    return x.replace(' ', '')
