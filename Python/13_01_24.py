597d61efbd90bf2255ceda69f198adfc7210bc57
Kaio Vinícios <kvsgpro@outlook.com>
2024-01-13 20:22:55 -0300

    Prática Python 13/01/2024.

---
+++ Python/13_01_24.py
@@ -0,0 +1,10 @@
+'''7 kyu "Friend or Foe?" by Aweson1
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...'''
+
+def friend(x):
+    friend = []
+    for n in range(0, len(x)): 
+        if len(x[n]) == 4:
+            friend.append(x[n])
+    return friend
